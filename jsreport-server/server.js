d600ab83b17fa689ebb566d7d06869d226851c7f
pofider <jan.blaha@hotmail.com>
2017-04-18 10:11:28 +0200

    use common jsreport server bootstrap

--- jsreport-server/server.js
+++ jsreport-server/server.js
@@ -1,24 +1,14 @@
-'use strict';
+var jsreport = require('jsreport')()
 
-const path = require('path');
-const fs = require('fs');
-
-const jsreport = require('jsreport')(
-  Object.assign({
-    rootDirectory: path.resolve(__dirname, '../'),
-    dataDirectory: path.join(__dirname, 'data')
-  }, JSON.parse(
-    fs.readFileSync(path.join(__dirname, 'dev.config.json').toString()
-  ))));
-
-const HOST_IP = process.env.HOST_IP;
-
-if (!HOST_IP) {
-  throw new Error('HOST_IP env var not found, please try setting HOST_IP env var before starting the server');
+if (require.main !== module) {
+  // export jsreport instance to make it possible to use jsreport-cli
+  module.exports = jsreport
+} else {
+  jsreport.init().then(function () {
+    // running
+  }).catch(function (e) {
+    // error during startup
+    console.error(e.stack)
+    process.exit(1)
+  })
 }
-
-jsreport.init().then(function () {
-  console.log('jsreport started')
-}).catch(function (err) {
-  throw new Error('jsreport could not start..,' + err.message)
-})
